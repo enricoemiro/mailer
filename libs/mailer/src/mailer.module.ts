ec8f81a3413c81670b8e393bcb80f8a30361a6c6
Enrico Emiro <16721212+enricoemiro@users.noreply.github.com>
2022-05-09 17:20:55 +0200

    chore: add forRoot method in MailerModule

--- libs/mailer/src/mailer.module.ts
+++ libs/mailer/src/mailer.module.ts
@@ -1,8 +1,22 @@
-import { Module } from '@nestjs/common';
+import { DynamicModule, Module, ValueProvider } from '@nestjs/common';
+
+import { MAILER_OPTIONS } from './mailer.constants';
+import { MailerModuleOptions, MailerTransport } from './mailer.interface';
 import { MailerService } from './mailer.service';
 
-@Module({
-  providers: [MailerService],
-  exports: [MailerService],
-})
-export class MailerModule {}
+@Module({})
+export class MailerModule {
+  static forRoot(options: MailerModuleOptions): DynamicModule {
+    const MailerOptionsProvider: ValueProvider<MailerTransport[]> = {
+      provide: MAILER_OPTIONS,
+      useValue: options.transports,
+    };
+
+    return {
+      module: MailerModule,
+      providers: [MailerOptionsProvider, MailerService],
+      exports: [MailerService],
+      global: options.global || false,
+    };
+  }
+}
