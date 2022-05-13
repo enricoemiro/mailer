69676c9919db137775d01c86816cd6de9ca182f5
Enrico Emiro <16721212+enricoemiro@users.noreply.github.com>
2022-05-13 21:16:06 +0200

    feat: implementing useExisting and useClass in MailerModule

--- lib/mailer.module.ts
+++ lib/mailer.module.ts
@@ -1,4 +1,5 @@
 import {
+  ClassProvider,
   DynamicModule,
   FactoryProvider,
   Module,
@@ -11,6 +12,7 @@ import {
   MailerModuleAsyncOptions,
   MailerModuleOptions,
   MailerTransport,
+  MailerTransportFactory,
 } from './mailer.interface';
 import { MailerService } from './mailer.service';
 
@@ -43,8 +45,19 @@ export class MailerModule {
   }
 
   private static createAsyncProviders(options: MailerModuleAsyncOptions) {
-    if (options.useFactory) {
-      return [this.createAsyncFactoryProvider(options)];
+    if (!options.useFactory && !options.useExisting && !options.useClass) {
+      throw new Error(
+        'Invalid forRootAsync configuration. Must provide useFactory, useExisting or useClass.',
+      );
+    }
+
+    if (options.useFactory) return [this.createAsyncFactoryProvider(options)];
+    if (options.useExisting) return [this.createAsyncCustomProvider(options)];
+    if (options.useClass) {
+      return [
+        this.createAsyncCustomProvider(options),
+        this.createAsyncClassProvider(options),
+      ];
     }
   }
 
@@ -55,6 +68,25 @@ export class MailerModule {
       provide: MAILER_OPTIONS,
       useFactory: options.useFactory,
       inject: options.inject || [],
+    };
+  }
+
+  private static createAsyncCustomProvider(
+    options: MailerModuleAsyncOptions,
+  ): FactoryProvider {
+    return {
+      provide: MAILER_OPTIONS,
+      useFactory: (f: MailerTransportFactory) => f.createMailerTransports(),
+      inject: [options.useClass || options.useExisting],
+    };
+  }
+
+  private static createAsyncClassProvider(
+    options: MailerModuleAsyncOptions,
+  ): ClassProvider {
+    return {
+      provide: options.useClass,
+      useClass: options.useClass,
     };
   }
 }
