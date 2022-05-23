5b2823e409ded0e161fc106f186ea0417568f079
Enrico Emiro <16721212+enricoemiro@users.noreply.github.com>
2022-05-23 20:02:16 +0200

    test: add e2e test

---
+++ tests/e2e.spec.ts
@@ -0,0 +1,44 @@
+import { DynamicModule, Provider } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+
+import { AppModule } from './src/app.module';
+
+async function createMockApplication(
+  options: DynamicModule,
+  extraProviders: Provider[] = [],
+) {
+  const module = await Test.createTestingModule({
+    imports: [options],
+    providers: [...extraProviders],
+  }).compile();
+
+  return module.createNestApplication().init();
+}
+
+describe('E2E tests', () => {
+  it('should be defined using forRoot()', async () => {
+    const app = await createMockApplication(AppModule.usingForRoot());
+
+    expect(
+      app.get(AppModule).mailerService.getTransporter('smtp'),
+    ).toBeDefined();
+  });
+
+  it('should be defined using forRootAsync()', async () => {
+    const app = await createMockApplication(AppModule.usingForRootAsync());
+
+    expect(
+      app.get(AppModule).mailerService.getTransporter('smtp'),
+    ).toBeDefined();
+  });
+
+  it('should be defined using forRootAsync() with use class', async () => {
+    const app = await createMockApplication(
+      AppModule.usingForRootAsyncWithUseClass(),
+    );
+
+    expect(
+      app.get(AppModule).mailerService.getTransporter('smtp'),
+    ).toBeDefined();
+  });
+});
